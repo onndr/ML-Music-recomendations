e3f1928dfb7182bde9d877384448d0574d24e46a
Andrii Gamalii <gamalii.andrii@gmail.com>
2024-01-05 01:14:20 +0100

    gosh it seems i understand how it works

--- src/target_model/model.py
+++ src/target_model/model.py
@@ -10,8 +10,8 @@ sessions = [[1, 2, 3, 4],
             [3, 5, 6, 7]]
 
 unique_items = list(set(item for session in sessions for item in session))
-item_to_index = {item: i for i, item in enumerate(unique_items)}
-index_to_item = {i: item for i, item in enumerate(unique_items)}
+item_to_index = {item: i for i, item in enumerate(unique_items, 1)}
+index_to_item = {i: item for i, item in enumerate(unique_items, 1)}
 
 sequences = [[item_to_index[item] for item in session] for session in sessions]
 
@@ -19,7 +19,7 @@ sequences = [[item_to_index[item] for item in session] for session in sessions]
 input_sequences = [seq[:-1] for seq in sequences]
 target_items = [seq[1:] for seq in sequences]
 
-padding_value = len(unique_items)
+padding_value = 0
 
 max_sequence_length = max(len(seq) for seq in input_sequences)
 padded_input_sequences = tf.keras.preprocessing.sequence.pad_sequences(input_sequences, padding='post', value=padding_value)
@@ -29,7 +29,7 @@ padded_target_items = tf.keras.preprocessing.sequence.pad_sequences(target_items
 embedding_dim = 50
 
 model = Sequential([
-    Embedding(input_dim=len(unique_items)+1, output_dim=embedding_dim, input_length=max_sequence_length),
+    Embedding(input_dim=len(unique_items)+1, output_dim=embedding_dim, input_length=max_sequence_length, mask_zero=True),
     GRU(100, activation='tanh', input_shape=(max_sequence_length, embedding_dim), unroll=True, return_sequences=True),
     Dense(len(unique_items)+1, activation='softmax')
 ])
@@ -38,19 +38,22 @@ model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=
 
 # Train the model
 # TODO: CHANGE TARGET ITEMS TO PADDED TARGET ITEMS
-model.fit(padded_input_sequences, np.array(target_items), epochs=15, batch_size=3)
+model.fit(padded_input_sequences, np.array(padded_target_items), epochs=50, batch_size=3)
 
 # Make predictions for a new session
-new_session = np.array([[2, 3]])
+new_session = np.array([[1, 5]])
 padded_new_session = tf.keras.preprocessing.sequence.pad_sequences(new_session, padding='post', maxlen=max_sequence_length)
 predictions = model.predict(padded_new_session)
-print(predictions)
+print("Padded input session: ", padded_new_session)
 # TODO: CHANGE PREDICTED ITEMS TO LIST OF PREDICTED ITEMS
 # Convert predictions to item indices
 # TODO: CHOOSE CORRECT SET OF PREDICTIONS
-predicted_items = np.argsort(predictions[0])[::-1][:3]
+predicted_items = [pred[::-1] for pred in np.argsort(predictions[0])[:padded_new_session.shape[1]]]
+
+predicted_items_last_step = predicted_items[-1]
 
 # Convert item indices to actual items
-recommended_items = [index_to_item[index] for index in predicted_items]
+# recommended_items = [[index_to_item[index] for index in pred_arr if index != padding_value] for pred_arr in predicted_items]
+recommended_items = [index_to_item[index] for index in predicted_items_last_step if index != padding_value]
 
 print("Recommended Items:", recommended_items)
