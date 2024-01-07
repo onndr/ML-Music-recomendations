8ff62d0839accaf92a15655c5f0a21a614f8efc0
Andrii Gamalii <gamalii.andrii@gmail.com>
2024-01-07 16:30:23 +0100

    refined base models

--- src/base_model/model.py
+++ src/base_model/model.py
@@ -1,4 +1,4 @@
-from src.data_prep.datapreprocessing import load_data, preprocess_sessions, preprocess_tracks, preprocess_users, save_data
+from src.data_prep.datapreprocessing import load_data
 import pandas as pd
 
 
@@ -6,9 +6,10 @@ class MostPopularTracksModel:
     def __init__(self):
         self._dataset = None
 
-    def train(self, tracks_dataset):
+    def train(self, tracks_dataset, n_to_hold=10):
         self._dataset = tracks_dataset
         self._dataset.sort_values(by='popularity', ascending=False, inplace=True)
+        self._dataset = self._dataset[:n_to_hold]
 
     def predict(self, n_tracks=10):
         return self._dataset['id'][:n_tracks].tolist()
@@ -17,66 +18,119 @@ class MostPopularTracksModel:
         if self._dataset is not None:
             self._dataset.to_json(path, orient='records', lines=True)
 
+    def load(self, path):
+        self._dataset = load_data(path)
+
 
 class MostListenedTracksModel:
     def __init__(self):
-        self.genre_track_counts = None
-        self.merged = None
+        self.counts_df = None
 
-    def train(self, users_dataset, sessions_dataset):
+    def train(self, users_dataset, sessions_dataset, tracks_for_genre=10):
+        # unpack favourite_genres list into separate rows
         users_df = users_dataset.explode('favourite_genres')
-        sessions_df = sessions_dataset
-        # Merge datasets
-        self.merged = pd.merge(sessions_df, users_df, on='user_id')
 
-        # Count the number of times each track is listened to by users with each favourite genre
-        self.genre_track_counts = self.merged.groupby(['favourite_genres', 'track_id']).size()
+        # basically a join on user_id
+        merged = pd.merge(sessions_dataset, users_df, on='user_id')
 
-    def predict(self, favourite_genre, n_tracks=10):
-        # Get the tracks with the highest count for the given genre
-        top_tracks = self.genre_track_counts[favourite_genre].nlargest(n_tracks).index.tolist()
+        # group by favourite_genres and track_id and count the number of occurrences
+        self.counts_df = merged.groupby(['favourite_genres', 'track_id'])
+        self.counts_df.as_index = False
+        self.counts_df = self.counts_df.size()
+
+        # sort by favourite_genres and count
+        self.counts_df = self.counts_df.sort_values(['favourite_genres', 'size'], ascending=[True, False])
+        self.counts_df = self.counts_df.groupby('favourite_genres').head(tracks_for_genre)
+
+        # rename columns
+        self.counts_df = self.counts_df.rename(columns={'favourite_genres': 'genre', 'size': 'count'})
+
+    def predict(self, genres, n_tracks=10):
+        # get the tracks with the highest count for the given genres
+        top_tracks = self.counts_df.loc[self.counts_df['genre'].isin(genres)].nlargest(n_tracks, columns='count')
         return top_tracks
 
-    def save(self, path_merged, path_genre_track_counts, n_best_tracks=10):
-        if self.merged.empty is not None and not self.merged.empty:
-            self.merged.to_json(path_merged, orient='records', lines=True)
-        if self.genre_track_counts is not None and not self.genre_track_counts.empty:
-            # Create an empty DataFrame to store the results
-            results = pd.DataFrame(columns=['genre', 'track_id', 'count'])
+    def save(self, path_genre_track_counts):
+        if self.counts_df is not None:
+            # save the df to jsonl
+            self.counts_df.to_json(path_genre_track_counts, orient='records', lines=True)
 
-            # For each genre, get the top tracks and add them to the results DataFrame
-            for genre in self.genre_track_counts.index.get_level_values('favourite_genres').unique():
-                top_tracks = self.genre_track_counts[genre].nlargest(n_best_tracks)
-                for track_id, count in top_tracks.items():
-                    new_row = pd.DataFrame({'genre': [genre], 'track_id': [track_id], 'count': [count]})
-                    results = pd.concat([results, new_row], ignore_index=True)
-
-            # Save the results DataFrame to JSON
-            results.to_json(path_genre_track_counts, orient='records', lines=True)
-
-    def load(self, merged_path):
-        self.merged = pd.read_json(merged_path, lines=True)
-        self.genre_track_counts = self.merged.groupby(['favourite_genres', 'track_id']).size()
+    def load(self, genre_track_counts_path):
+        # load the data from jsonl
+        self.counts_df = pd.read_json(genre_track_counts_path, lines=True)
 
 
-def main():
+def test_most_popular_tracks_model_train(tracks_path, save_model_path, n_mp_tracks, n_tracks_to_predict):
     # Load data
-    # sessions_df = load_data('../../data/sessions.jsonl')
-    # sessions_df = sessions_df[sessions_df['event_type'] == 'play'][['user_id', 'track_id', 'session_id']]
-    # users_df = load_data('../../data/users.jsonl')[['user_id', 'favourite_genres']]
+    tracks_df = load_data(tracks_path)[['id', 'name', 'popularity']]
+
+    # Train model
+    most_popular_tracks_model = MostPopularTracksModel()
+    most_popular_tracks_model.train(tracks_df, n_mp_tracks)
+    most_popular_tracks_model.save(save_model_path)
+
+    # Predict
+    most_popular_tracks = most_popular_tracks_model.predict(n_tracks_to_predict)
+    print(f'Most popular tracks: {most_popular_tracks}')
+
+
+def test_most_popular_tracks_model_load(saved_model_path, n_tracks_to_predict):
+    # Load model
+    most_popular_tracks_model = MostPopularTracksModel()
+    most_popular_tracks_model.load(saved_model_path)
+
+    # Predict
+    most_popular_tracks = most_popular_tracks_model.predict(n_tracks_to_predict)
+    print(f'Most popular tracks: {most_popular_tracks}')
+
+
+def test_most_listened_tracks_model_train(sessions_path, clean_sessions_path, users_path,
+                                          save_model_path, n_tracks_per_genre, genres):
+    # Clean data
+    # clean_sessions(sessions_path, clean_sessions_path)
+
+    # Load data
+    sessions_df = load_data(clean_sessions_path)[['user_id', 'track_id']]
+    users_df = load_data(users_path)[['user_id', 'favourite_genres']]
 
     # Train model
     most_listened_tracks_model = MostListenedTracksModel()
-    most_listened_tracks_model.load('../../data/merged.jsonl')
-
-    # Save model
-    most_listened_tracks_model.save('../../data/merged.jsonl', '../../data/most_listened_tracks_model.jsonl')
+    most_listened_tracks_model.train(users_df, sessions_df, n_tracks_per_genre)
+    most_listened_tracks_model.save(save_model_path)
 
     # Predict
-    favourite_genre = 'rock'
-    n_tracks = 10
-    most_listened_tracks = most_listened_tracks_model.predict(favourite_genre, n_tracks)
-    print(f'Most listened tracks for genre {favourite_genre}: {most_listened_tracks}')
+    most_listened_tracks = most_listened_tracks_model.predict(genres)
+    print(f'Most listened tracks for genres {genres}: {most_listened_tracks}')
+
+
+def test_most_listened_tracks_model_load(saved_model_path, genres):
+    # Load model
+    most_listened_tracks_model = MostListenedTracksModel()
+    most_listened_tracks_model.load(saved_model_path)
+
+    # Predict
+    most_listened_tracks = most_listened_tracks_model.predict(genres)
+    print(f'Most listened tracks for genres {genres}: {most_listened_tracks}')
+
+
+def main():
+    SESSIONS_PATH = '../../data/sessions.jsonl'
+    CLEAN_SESSIONS_PATH = '../../data/sessions_clean.jsonl'
+    TRACKS_PATH = '../../data/tracks.jsonl'
+    USERS_PATH = '../../data/users.jsonl'
+    MOST_POPULAR_MODEL_PATH = './saved_models/most_popular_model.jsonl'
+    MOST_LISTENED_MODEL_PATH = './saved_models/most_listened_model.jsonl'
+
+    N_TRACKS_TO_PREDICT = 10
+    N_POPULAR_TRACKS_TO_SAVE = 100
+    N_TRACKS_PER_GENRE = 10
+    GENRES = ['rock', 'pop punk']
+
+    test_most_popular_tracks_model_train(TRACKS_PATH, MOST_POPULAR_MODEL_PATH, N_POPULAR_TRACKS_TO_SAVE, N_TRACKS_TO_PREDICT)
+    test_most_popular_tracks_model_load(MOST_POPULAR_MODEL_PATH, N_TRACKS_TO_PREDICT)
+    test_most_listened_tracks_model_train(SESSIONS_PATH, CLEAN_SESSIONS_PATH, USERS_PATH, MOST_LISTENED_MODEL_PATH,
+                                          N_TRACKS_PER_GENRE, GENRES)
+    test_most_listened_tracks_model_load(MOST_LISTENED_MODEL_PATH, GENRES)
 
 
 if __name__ == '__main__':
--- src/data_prep/datapreprocessing.py
+++ src/data_prep/datapreprocessing.py
@@ -1,6 +1,13 @@
 import pandas as pd
 
 
+def clean_sessions(sessions_path, sessions_clean_path):
+    sessions_df = load_data(sessions_path)
+    sessions_df = sessions_df[sessions_df['event_type'] == 'play']
+    sessions_df = sessions_df.drop('event_type', axis=1)
+    sessions_df.to_json(sessions_clean_path, orient='records', lines=True)
+
+
 def load_data(path):
     return pd.read_json(path, lines=True)
 
@@ -16,13 +23,3 @@ def preprocess_sessions(sessions_df):
     sessions_df['user_id'] = sessions_df['user_id'].apply(lambda x: x[0])
     sessions_df = sessions_df.rename(columns={'track_id': 'tracks_ids'})
     return sessions_df
-
-
-def preprocess_tracks(tracks_df):
-    tracks_df = tracks_df[['id', 'name', 'popularity']]
-    return tracks_df
-
-
-def preprocess_users(users_df):
-    users_df = users_df[['user_id', 'favourite_genres']]
-    return users_df
