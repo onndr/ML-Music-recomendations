3107aca8dc64900737656a45344323ab216bff31
ategloc <wik.topolski@gmail.com>
2024-01-19 22:02:59 +0100

    fix

--- src/app/main.py
+++ src/app/main.py
@@ -5,27 +5,22 @@ from src.base_model.model import MostListenedTracksModel, MostPopularTracksModel
 
 app = Flask(__name__)
 
-import pickle
-
 model_knn = KNNModel()
 model_popular = MostPopularTracksModel()
 model_most_listened = MostListenedTracksModel()
 
 try:
-    with open('knn_model.pkl', 'rb') as file:
-        model_knn = model_knn.load_model(file)
+    model_knn = model_knn.load_model('knn_model.pkl')
 except:
     pass
 
 try:
-    with open('popular_model.pkl', 'rb') as file:
-        model_popular = model_popular.load(file)
+    model_popular = model_popular.load('most_popular_model.jsonl')
 except:
-    pass   
+    pass
 
 try:
-    with open('listened_model.pkl', 'rb') as file:
-        model_most_listened = model_most_listened.load(file)
+    model_most_listened = model_most_listened.load('most_listened_model.jsonl')
 except:
     pass
 
@@ -36,6 +31,7 @@ def make_a_ids_list(df):
         ids.append(df.loc[i]['id'])
     return ids
 
+
 @app.route('/predict/knn', methods=['POST'])
 def predict_knn():
     try:
@@ -43,30 +39,27 @@ def predict_knn():
         songs = request.json['songs']
         songs = pd.DataFrame(songs)
         merged_song = model_knn.avg_song(songs)
-
-
         # Make predictions
         predictions = model_knn.predict(merged_song)
 
         # Return the top N recommendations
-
         return jsonify({'recommendations': make_a_ids_list(predictions)})
 
     except Exception as e:
         return jsonify({'error': str(e)})
-    
+
 
 @app.route('/predict/popular', methods=['POST'])
 def predict_popular():
-    try:
-    
+    # try:
         predictions = model_popular.predict(10)
 
         return jsonify({'recommendations': make_a_ids_list(predictions)})
 
-    except Exception as e:
-        return jsonify({'error': str(e)})
-    
+    # except Exception as e:
+    #     return jsonify({'error': str(e)})
+
+
 @app.route('/predict/most_listened', methods=['POST'])
 def predict_most_listened():
     try:
@@ -79,6 +72,7 @@ def predict_most_listened():
 
     except Exception as e:
         return jsonify({'error': str(e)})
+
 
 @app.route('/train/knn', methods=['POST'])
 def train_knn():
@@ -93,6 +87,7 @@ def train_knn():
     except Exception as e:
         return jsonify({'error': str(e)})
 
+
 @app.route('/train/popular', methods=['POST'])
 def train_popular():
     try:
@@ -104,29 +99,34 @@ def train_popular():
 
     except Exception as e:
         return jsonify({'error': str(e)})
-    
+
+
 @app.route('/train/most_listened', methods=['POST'])
 def train_most_listened():
     try:
         users = pd.DataFrame(request.json['users'])
         sessions = pd.DataFrame(request.json['sessions'])
         model_most_listened.train(users, sessions, 10)
-        
+
 
     except Exception as e:
         return jsonify({'error': str(e)})
+
 
 @app.route('/save/knn', methods=['POST'])
 def save_knn():
     model_knn.save_model('models/knn_model.pkl')
 
+
 @app.route('/save/popular', methods=['POST'])
 def save_popular():
-    model_popular.save('models/popular_model.pkl')
+    model_popular.save('models/most_popular_model.jsonl')
+
 
 @app.route('/save/most_listened', methods=['POST'])
 def save_most_listened():
-    model_most_listened.save('models/listened_model.pkl')
+    model_most_listened.save('models/listened_model.jsonl')
+
 
 @app.route('/predict/abc', methods=['POST'])
 def predict_abc():
@@ -147,8 +147,6 @@ def predict_abc():
 
     except Exception as e:
         return jsonify({'error': str(e)})
-
-
 
 
 if __name__ == '__main__':
